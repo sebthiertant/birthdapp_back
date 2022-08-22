ce0de40db73bc90c6990982bf072af406c387b9f
SebThiertant <sebastien.thiertant@gmail.com>
2022-08-22 12:12:30 +0200

    First commit

---
+++ src/main/java/com/example/birthdapp_back/BirthdappBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.birthdapp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdappBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdappBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/birthdapp_back/BirthdappBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.birthdapp_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdappBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
