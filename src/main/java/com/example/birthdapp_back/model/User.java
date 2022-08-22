60ee9c72e1fe2f629e6431932fd20bd10078437a
Steven Prieur <steven.prieur@hotmail.fr>
2022-08-22 16:34:00 +0200

    Création du package model + création de la classe User

---
+++ src/main/java/com/example/birthdapp_back/model/User.java
@@ -0,0 +1,48 @@
+package com.example.birthdapp_back.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
