e8dcf88b946fa6a28497b0baa804a3009e449fcb
SebThiertant <sebastien.thiertant@gmail.com>
2022-08-23 10:22:15 +0200

    Ajout du package repository, et création des deux interfaces (vides)

--- src/main/java/com/example/birthdapp_back/BirthdappBackApplication.java
+++ src/main/java/com/example/birthdapp_back/BirthdappBackApplication.java
@@ -3,11 +3,11 @@ package com.example.birthdapp_back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BirthdappBackApplication {
 
     public static void main(String[] args) {
-        //System.out.println("Test");
         SpringApplication.run(BirthdappBackApplication.class, args);
     }
 }
---
+++ src/main/java/com/example/birthdapp_back/repository/BirthdayRepository.java
@@ -0,0 +1,10 @@
+package com.example.birthdapp_back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.birthdapp_back.model.Birthday;
+
+@Repository
+public interface BirthdayRepository extends JpaRepository<Birthday, Long> {
+}
