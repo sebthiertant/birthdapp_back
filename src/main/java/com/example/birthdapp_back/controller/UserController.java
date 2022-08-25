674cad5b4aec73b128a9b2d00b2534ce952e08d0
Steven Prieur <steven.prieur@hotmail.fr>
2022-08-25 10:22:56 +0200

    Finalisation du userController

--- src/main/java/com/example/birthdapp_back/controller/UserController.java
+++ src/main/java/com/example/birthdapp_back/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.example.birthdapp_back.controller;
 
+import com.example.birthdapp_back.model.Birthday;
 import com.example.birthdapp_back.model.User;
 import com.example.birthdapp_back.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -16,14 +16,40 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private BirthdayService birthdayService;
+
 
     @GetMapping(value = {"", "/"})
-    public List<User> getUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/{userId}")
-    public User getUserById(@PathVariable("userId") Long id) {
-        return userService.getUserByID(id);
+    public ResponseEntity<User> getUserById(@PathVariable("userId") Long id) {
+        try {
+            User user = userService.getUserByID(id);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
+
+    @PostMapping("/{userId}/birthdays")
+    public ResponseEntity<Birthday> createBirthday(
+            @PathVariable("userId") Long id,
+            @RequestParam("firstname") final String firstname,
+            @RequestParam("lastname") final String lastname,
+            @RequestParam("date") final String date) {
+
+        try {
+            User user = userService.getUserByID(id);
+            Birthday birthday = new Birthday(null, LocalDate.parse(date), firstname, lastname, user);
+            return ResponseEntity.ok(birthdayService.save(birthday));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
 }
